4c21fd2e3bf6435c511a8aecb05dee24fb0fbbaf
Moon <moon.kyong@automattic.com>
2021-02-04 01:50:58 -0800

    saving for tmr -- this will be squashed

--- docker/wc-admin-docker-dev/cli.js
+++ docker/wc-admin-docker-dev/cli.js
@@ -2,28 +2,13 @@
  * External dependencies
  */
 const yargs = require( 'yargs' ).strict();
-const path = require( 'path' );
-const execSync = require( 'child_process' ).execSync;
-const cwd = require( 'path' ).dirname( require.main.filename );
-const crypto = require( 'crypto' );
 
 /**
  * Internal dependencies
  **/
 const commands = require( './commands' );
 const run = require( './run' );
-
-const configPath = path.resolve( `${ cwd }/../.wp-env.json` );
-const wpEnvHash = crypto
-	.createHash( 'md5' )
-	.update( configPath )
-	.digest( 'hex' );
-
-const containerId = execSync(
-	`docker ps -q --filter="name=^${ wpEnvHash }_wordpress"`
-)
-	.toString()
-	.trim();
+const containerId = require( './container-id' );
 
 module.exports = function cli() {
 	for ( const i in commands ) {
