bd03984473ecad53e0372977a88757c9024ee468
Moon <moon.kyong@automattic.com>
2021-02-03 22:20:48 -0800

    saving for tmr -- this will be squashed

--- docker/wc-admin-docker-dev/cli.js
+++ docker/wc-admin-docker-dev/cli.js
@@ -6,15 +6,19 @@ const yargs = require( 'yargs' );
 const path = require( 'path' );
 const execSync = require( 'child_process' ).execSync;
 const cwd = require( 'path' ).dirname( require.main.filename );
+const crypto = require( 'crypto' );
 
 /**
  * Internal dependencies
  **/
-const md5 = require( './md5' );
 const commands = require( './commands' );
 
 const configPath = path.resolve( `${ cwd }/../.wp-env.json` );
-const wpEnvHash = md5( configPath );
+const wpEnvHash = crypto
+	.createHash( 'md5' )
+	.update( configPath )
+	.digest( 'hex' );
+
 const containerId = execSync(
 	`docker ps -q --filter="name=^${ wpEnvHash }_wordpress"`
 )
